28d28a64d22c26dce045e6183fe2230f07ec736b
Comit <fedor.palexov2005@gmail.com>
2020-02-28 18:55:07 +0300

    Azaza! AA? AAAZZZAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA

--- app/src/main/java/com/banana/b19/promo/MainActivity.kt
+++ app/src/main/java/com/banana/b19/promo/MainActivity.kt
@@ -2,12 +2,35 @@ package com.banana.b19.promo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewPage.adapter = Adapter()
+        tabLayout.setupWithViewPager(viewPage)
+    }
+inner class Adapter: FragmentPagerAdapter(supportFragmentManager,
+    BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
+{
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> PageFrament()
+        else ->PageFrament()}
     }
 
+
+    override fun getCount(): Int  = 10
+
+
+
+
+
 }
+
+}
+
---
+++ app/src/main/java/com/banana/b19/promo/PageFrament.kt
@@ -0,0 +1,34 @@
+package com.banana.b19.promo
+
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PageFrament : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_page_frament, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.setBackgroundColor( getrandomColor())
+
+    }
+    private fun getrandomColor()= Color.rgb((0..255).random(),(0..255).random(),(0..255).random())
+
+
+
+
+}
